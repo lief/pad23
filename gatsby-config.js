ef31533e05f686a2a253b13d4de1322fc6b7e86c
Lief van der Baan <lief@hey.com>
2022-09-16 18:50:43 +0100

    Changed a color.

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
         short_name: `GatsbyJS & WP`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#866b3c`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
