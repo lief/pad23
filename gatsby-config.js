5fbc4d0cceae7f4259009e8cceb0d1c58b4ba5e8
Lief van der Baan <lief@hey.com>
2022-09-16 18:53:46 +0100

    Filled out the manifest, now I get it.

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,8 +58,8 @@ module.exports = {
       // See https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/?=gatsby-plugin-manifest
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter WordPress Blog`,
-        short_name: `GatsbyJS & WP`,
+        name: `pad23`,
+        short_name: `pad23`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#866b3c`,
